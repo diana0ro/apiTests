8f10ff90b2086c1cca7619b26d444dad9186f909
diana0ro <72491435+diana0ro@users.noreply.github.com>
2025-03-09 17:24:03 +0200

    remve testcontainer

--- test_attempt.py
+++ test_attempt.py
@@ -1,9 +1,9 @@
 import requests
-from testcontainers.core.container import DockerContainer
+#from testcontainers.core.container import DockerContainer
 
 def test_get_users():
     # Send a GET request to the API
-    container = DockerContainer("server:1.0")
+  #  container = DockerContainer("server:1.0")
    # port = container.get_exposed_port(80)
     response = requests.get(f"http://localhost/#/store/getInventory")
         
